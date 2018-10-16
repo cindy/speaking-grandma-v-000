e0acb6f865bb386734287f6b0520413501eb8361
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-10-16 02:13:35 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(words)
   if words == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif words == words.capitalize
+  elsif words == words.upcase
     return "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
